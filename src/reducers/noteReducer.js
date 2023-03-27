e8434b12cefeedb02963137572141dc53ca2fb64
Austin Wang <yyaustin@bu.edu>
2023-03-27 01:10:18 -0400

    Setup Redux Toolkit

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 const initialState = [
   {
     content: 'reducer defines how redux store works',
@@ -11,11 +13,22 @@ const initialState = [
   }
 ]
 
-const noteReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case "NEW_NOTE":
-      return state.concat(action.payload)
-    case "TOGGLE_IMPORTANCE":
+const generateId = () => Number((Math.random() * 100000).toFixed(0))
+
+const noteSlice = createSlice({
+  name: "notes",
+  initialState,
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload
+      // Immer
+      state.push({
+        content,
+        important: false,
+        id: generateId()
+      })
+    },
+    toggleImportanceOf(state, action) {
       const id = action.payload.id
       const noteToChange = state.find(note => note.id === id)
       const changedNote = {
@@ -23,30 +36,9 @@ const noteReducer = (state = initialState, action) => {
         important: !noteToChange.important
       }
       return state.map(note => note.id === id ? changedNote : note)
-    default:
-      return state
-  }
-}
-
-const generateId = () => Number((Math.random() * 100000).toFixed(0))
-
-/* actions */
-export const createNote = (content) => {
-  return {
-    type: "NEW_NOTE",
-    payload: {
-      content,
-      important: false,
-      id: generateId()
     }
   }
-}
+})
 
-export const toggleImportanceOf = (id) => {
-  return {
-    type: "TOGGLE_IMPORTANCE",
-    payload: { id }
-  }
-}
-
-export default noteReducer
+export const { createNote, toggleImportanceOf } = noteSlice.actions
+export default noteSlice.reducer
