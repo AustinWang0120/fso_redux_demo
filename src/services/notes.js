efa7d0465788d1962d8da5f34c9081b6dc201827
Austin Wang <yyaustin@bu.edu>
2023-04-02 10:56:53 -0400

    Connect Redux and backend

---
+++ src/services/notes.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+const baseUrl = "http://localhost:3001/notes"
+
+const getAll = async () => {
+  const res = await axios.get(baseUrl)
+  return res.data
+}
+
+const createNew = async (content) => {
+  const object = {
+    content,
+    important: false
+  }
+  const res = await axios.post(baseUrl, object)
+  return res.data
+}
+
+const myService = {
+  getAll, createNew
+}
+
+export default myService
